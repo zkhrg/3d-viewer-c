33fa41cb2f02dd809e8fa70e967c8a66959425ad
Diamond Phanes <zakhar.griaznov@gmail.com>
2024-07-30 15:33:41 +0300

    parsers?

--- dot_obj_parser.c
+++ dot_obj_parser.c
@@ -22,8 +22,51 @@ int parse_dot_obj_file(char* filename, dot_obj_data* dod) {
 }
 
 int parse_v_and_f(FILE* f, dot_obj_data* dod) {
-  ;
-  ;
+  int parse_status = OK;
+  char* line = NULL;
+  size_t len = 0;
+  size_t i = 0;
+  size_t v_counter = 0;
+  size_t f_counter = 0;
+
+  while (getline(&line, &len, f) != -1) {
+    if (strncmp(line, "v ", 2) == 0) {
+      double x, y, z;
+      sscanf(line, "v %lf %lf %lf", &x, &y, &z);
+      dod->vertices[i++] = x;
+      dod->vertices[i++] = y;
+      dod->vertices[i++] = z;
+      v_counter++;
+    } else if (strncmp(line, "f ", 2) == 0) {
+      char* token = strtok(line + 2, " ");
+      int vertex_index;
+      int first_index = -1;
+
+      while (token != NULL) {
+        if (sscanf(token, "%d/%*d/%*d", &vertex_index) == 1) {
+          if (vertex_index < 0) {
+            vertex_index += v_counter + 1;
+          }
+          if (first_index == -1) {
+            first_index = vertex_index - 1;
+          }
+          dod->faces[f_counter++] = vertex_index - 1;
+        }
+
+        token = strtok(NULL, " ");
+      }
+
+      if (first_index != -1 && f_counter > 0 && f_counter % 2 == 0) {
+        dod->faces[f_counter++] = first_index;
+      }
+    }
+  }
+
+  if (line) {
+    free(line);
+  }
+
+  return parse_status;
 }
 
 int dodalloc(dot_obj_data* dod) {
