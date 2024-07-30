968cc2f041a210661ae32b6d50d0a3deff84f202
Diamond Phanes <zakhar.griaznov@gmail.com>
2024-07-30 16:47:57 +0300

    parser maybe working

--- dot_obj_parser.c
+++ dot_obj_parser.c
@@ -72,7 +72,7 @@ int parse_v_and_f(FILE* f, dot_obj_data* dod) {
 int dodalloc(dot_obj_data* dod) {
   int status = OK;
   dod->vertices = (double*)malloc(dod->v_count * 3 * sizeof(double));
-  dod->faces = (int*)malloc(dod->f_count * 3 * sizeof(int));
+  dod->faces = (int*)malloc(dod->f_count * sizeof(int));
 
   if (!dod->vertices || !dod->faces) {
     status = ERROR;
@@ -86,28 +86,62 @@ int dodalloc(dot_obj_data* dod) {
 
 void count_v_and_f(FILE* f, dot_obj_data* dod) {
   size_t vertex_count = 0;
-  size_t face_count = 0;
+  size_t edge_count = 0;  // Количество рёбер (границ)
 
   char* line = NULL;
   size_t len = 0;
 
   while (getline(&line, &len, f) != -1) {
-    line[strcspn(line, "\n")] = '\0';
+    line[strcspn(line, "\n")] = '\0';  // Удаление символа новой строки
 
     if (line[0] == '#' || line[0] == '\0') {
-      continue;
+      continue;  // Пропуск комментариев и пустых строк
     }
 
-    if (line[0] == 'v') {
-      if (line[1] == ' ') {
-        vertex_count++;
-      }
+    if (line[0] == 'v' && line[1] == ' ') {
+      vertex_count++;  // Подсчет вершин
     } else if (line[0] == 'f' && line[1] == ' ') {
-      face_count++;
+      // Подсчет рёбер
+      size_t count = 0;
+      char* token = strtok(line + 2, " ");  // Пропускаем 'f '
+
+      while (token != NULL) {
+        count++;
+        token = strtok(NULL, " ");
+      }
+
+      // Количество рёбер в многоугольнике
+      // В многоугольнике количество рёбер = количество вершин
+      edge_count += count;  // Это количество рёбер для текущей строки
     }
   }
+
   free(line);
 
   dod->v_count = vertex_count;
-  dod->f_count = face_count;
+  dod->f_count = edge_count;
+
+  printf("Number of vertices: %ld\n", vertex_count);
+  printf("Number of edges: %ld\n", edge_count);
+}
+
+void print_dod(dot_obj_data* dod) {
+  // printing vertices
+  printf("Amount of vertex: %ld\n", dod->v_count);
+  for (int i = 0; i < (int)dod->v_count * 3; i++) {
+    printf("%lf ", dod->vertices[i]);
+    if ((i + 1) % 3 == 0) {
+      printf("\n");
+    }
+  }
+  printf("\n");
+  // printing vertex_indices
+  printf("Amount of vertex_indices: %ld\n", dod->f_count);
+  for (int i = 0; i < (int)dod->f_count; i++) {
+    if (i + 1 < (int)dod->f_count) {
+      printf("%d %d ", dod->faces[i], dod->faces[i + 1]);
+      printf("\n");
+    }
+  }
+  printf("%d %d\n", dod->faces[dod->f_count - 1], dod->faces[0]);
 }
--- dot_obj_parser.h
+++ dot_obj_parser.h
@@ -6,5 +6,6 @@ int parse_dot_obj_file(char* filename, dot_obj_data* dod);
 void count_v_and_f(FILE* f, dot_obj_data* dod);
 int parse_v_and_f(FILE* f, dot_obj_data* dod);
 int dodalloc(dot_obj_data* dod);
+void print_dod(dot_obj_data* dod);
 
 #endif  // DOT_OBJ_PARSER_H
